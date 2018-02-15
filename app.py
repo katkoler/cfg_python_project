7bd3d1b41bc92d881cf1b1c50c691346b65b3511
Kat Koler <katjusa.koler@gmail.com>
2018-02-15 10:00:55 +0000

    make it pink

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask("my_first_app")
 
 @app.route("/")
 def say_hello():
-  return "hello world!"
+  return render_template("index.html")
+
+@app.route('/hello')
+def hello():
+    return 'Hi'
+
+@app.route('/<number1>/<number2>') #captures whatever you put in <x> 
+def add_number(number1, number2):
+	result = int(number1) + int(number2)
+	return render_template("index.html", result=result)
+
 
 app.run(debug=True)
+
+
